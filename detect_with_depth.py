64164aa6ecbf0019b8f4aad526883544c127bf12
liupenglei <52022245+liupenglei@users.noreply.github.com>
2025-08-26 10:13:52 +0800

    Add files via upload

---
+++ detect_with_depth.py
@@ -0,0 +1,172 @@
+import cv2
+import numpy as np
+import pyrealsense2 as rs
+import os
+import json
+import utils
+import shutil
+
+# 读取 Aruco Board 配置
+config_path = os.path.join(os.getcwd(), 'configs', 'aruco', 'sl200_ml120.json')
+with open(config_path, 'r') as f:
+    cfg = json.load(f)
+
+sl = cfg['sl']
+ml = cfg['ml']
+ids = cfg['ids']
+charucodict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+board = cv2.aruco.CharucoBoard((3, 3), sl, ml, charucodict, np.array([ids]))
+
+axis_offset = -int(sl * 3 / 2)
+origin_offset = np.array([[axis_offset+00, axis_offset, 0]])
+Rx180 = np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
+
+x, y, z, c = 100, 100, 200, 0
+bbox3d = None
+
+# Realsense 配置
+pipe = rs.pipeline()
+rscfg = rs.config()
+width, height = 1280, 720
+rscfg.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
+rscfg.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
+profile = pipe.start(rscfg)
+
+# 获取 Realsense 内参
+intrin = profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
+K_full = np.array([[intrin.fx, 0, intrin.ppx], [0, intrin.fy, intrin.ppy], [0, 0, 1]])
+
+# 创建数据保存目录
+dataset_name = "recorded_dataset123"
+
+
+dataset_pth = os.path.join(os.getcwd(), 'out', 'datasets', dataset_name)
+if os.path.exists(dataset_pth):
+    shutil.rmtree(dataset_pth)
+os.makedirs(dataset_pth)
+os.makedirs(os.path.join(dataset_pth, 'color'))
+os.makedirs(os.path.join(dataset_pth, 'raw_color'))
+os.makedirs(os.path.join(dataset_pth, 'raw_depth'))
+os.makedirs(os.path.join(dataset_pth, 'intrin_ba'))
+os.makedirs(os.path.join(dataset_pth, 'poses_ba'))
+os.makedirs(os.path.join(dataset_pth, 'reproj_box'))
+
+# 视频录制设置
+frame_interval = 1      # 每 5 帧保存一次
+frame_count    = 0
+idx            = 0
+recording      = False  # 录制状态
+
+video_path     = os.path.join(dataset_pth, 'video.avi')
+raw_video_path = os.path.join(dataset_pth, 'raw_video.avi')
+fourcc         = cv2.VideoWriter_fourcc(*'XVID')
+fps            = 15
+
+video_writer     = cv2.VideoWriter(video_path,     fourcc, fps, (512, 512))
+raw_video_writer = cv2.VideoWriter(raw_video_path, fourcc, fps, (512, 512))
+
+
+while True:
+    bbox3d = np.array([
+        [x, y, c], [x, y, z + c], [x, -y, z + c], [x, -y, c],
+        [-x, y, c], [-x, y, z + c], [-x, -y, z + c], [-x, -y, c]
+    ])
+
+    # 读取帧
+    frameset = pipe.wait_for_frames()
+    color_frame = frameset.get_color_frame()
+    bgr = np.asanyarray(color_frame.get_data())
+    frame_count += 1
+
+
+    # 处理裁剪后的图像
+    bbox = np.array([int(width / 2 - height / 2), 0, width - int(width / 2 - height / 2), height])
+    bgr_crop, K_crop    = utils.crop_img_by_bbox(bgr, bbox, K=K_full, crop_size = 512)
+    bgr_crop_raw, K_raw = utils.crop_img_by_bbox(bgr, bbox, K=K_full, crop_size = 512)
+    markerCorners, markerIds, _ = cv2.aruco.detectMarkers(bgr_crop, charucodict)
+    cv2.aruco.drawDetectedMarkers(bgr_crop, markerCorners, markerIds)
+
+    if markerIds is not None:
+        objPoints, imgPoints = board.matchImagePoints(markerCorners, markerIds)
+        objPoints = objPoints + origin_offset
+        _, rvec, tvec = cv2.solvePnP(objPoints, imgPoints, K_crop, distCoeffs=None)
+        rmat = cv2.Rodrigues(rvec)[0] @ Rx180
+        rvec = cv2.Rodrigues(rmat)[0]
+
+        pose = np.eye(4)
+        pose[:3, :3] = rmat
+        pose[:3, 3] = tvec[:, 0]
+        
+
+        bbox2d = utils.reproj(K_crop, pose, bbox3d)
+        utils.draw_3d_box(bgr_crop, bbox2d)
+        cv2.drawFrameAxes(bgr_crop, K_crop, None, rvec, tvec, 100)
+
+        # 录制时保存数据
+        if recording and frame_count % frame_interval == 0:
+            print(f'保存数据，帧 {idx}')
+            
+            # 未标注图片
+            bgr_crop_clean, K_crop_clean = utils.crop_img_by_bbox(bgr, bbox, K=K_full, crop_size = 512)
+            cv2.imwrite(os.path.join(dataset_pth, 'raw_color', f'{idx}.png'), bgr_crop_clean)
+            
+
+            # 保存深度图
+            depth_frame = frameset.get_depth_frame()
+            depth_image = np.asanyarray(depth_frame.get_data())
+            depth_crop, _ = utils.crop_img_by_bbox(depth_image, bbox, K=None, crop_size=512)
+            cv2.imwrite(os.path.join(dataset_pth, 'raw_depth', f'{idx}_depth.png'), depth_crop)
+
+
+            # 标注图片
+            cv2.imwrite(os.path.join(dataset_pth, 'color', f'{idx}.png'), bgr_crop)
+            
+            # 标注数据
+            np.savetxt(os.path.join(dataset_pth, 'intrin_ba', f'{idx}.txt'), K_crop)
+            np.savetxt(os.path.join(dataset_pth, 'poses_ba', f'{idx}.txt'), pose)
+            np.savetxt(os.path.join(dataset_pth, 'reproj_box', f'{idx}.txt'), bbox2d)
+
+            # 录制视频
+            video_writer.write(bgr_crop)
+            
+			# 保存未标注视频
+            raw_video_writer.write(bgr_crop_raw)
+            
+            idx += 1
+
+    # 显示处理后的画面
+    cv2.imshow('frame_crop', bgr_crop)
+    key = cv2.waitKey(1)
+
+    # 按键交互
+    if key == ord('q'):
+        break
+    elif key == ord('w'):
+        origin_offset[0, 2] += 10
+        c -= 10
+    elif key == ord('s'):
+        origin_offset[0, 2] -= 10
+        c += 10
+    elif key == ord('e'):
+        x += 10
+    elif key == ord('d'):
+        x -= 10
+    elif key == ord('r'):
+        y += 10
+    elif key == ord('f'):
+        y -= 10
+    elif key == ord('t'):
+        z += 10
+    elif key == ord('g'):
+        z -= 10
+    elif key == 13:  # 回车键
+        recording = not recording
+        print("开始录制..." if recording else "暂停录制...")
+
+# 释放资源
+pipe.stop()
+video_writer.release()
+raw_video_writer.release()
+cv2.destroyAllWindows()
+np.savetxt(os.path.join(dataset_pth, 'box3d_corners.txt'), bbox3d)
+print("录制完成，数据已保存。")
---
+++ utils.py
@@ -0,0 +1,175 @@
+import cv2
+import numpy as np
+
+def reproj(K, pose, pts_3d):
+    assert K.shape == (3, 3) or K.shape == (3, 4)
+    assert pose.shape == (3, 4) or pose.shape == (4, 4)
+
+    if K.shape == (3, 3):
+        K_homo = np.concatenate([K, np.zeros((3, 1))], axis=1)
+    else:
+        K_homo = K
+    
+    if pose.shape == (3, 4):
+        pose_homo = np.concatenate([pose, np.array([[0, 0, 0, 1]])], axis=0)
+    else:
+        pose_homo = pose
+    
+    pts_3d = pts_3d.reshape(-1, 3)
+    pts_3d_homo = np.concatenate([pts_3d, np.ones((pts_3d.shape[0], 1))], axis=1)
+    pts_3d_homo = pts_3d_homo.T
+
+    reproj_points = K_homo @ pose_homo @ pts_3d_homo
+    reproj_points = reproj_points[:] / reproj_points[2:]
+    reproj_points = reproj_points[:2, :].T
+    return reproj_points # [n, 2]
+    
+def draw_3d_box(image, corners_2d, linewidth=2, color='b'):
+    lines = np.array([
+        [0, 1, 5, 4, 2, 3, 7, 6, 2, 2, 3, 7],
+        [1, 5, 4, 0, 3, 0, 6, 5, 1, 6, 7, 4]
+    ]).T
+
+    colors = {
+        'g': (0, 255, 0),
+        'r': (0, 0, 255),
+        # 'b': (255, 0, 0)
+		'b': (155, 100, 30)
+    }
+    if color not in colors.keys():
+        color = (42, 97, 247)
+    else:
+        color = colors[color]
+    
+    for id, line in enumerate(lines):
+        pt1 = corners_2d[line[0]].astype(int)
+        pt2 = corners_2d[line[1]].astype(int)
+        cv2.line(image, tuple(pt1), tuple(pt2), color, linewidth)
+
+def rect_to_square(self, x0, y0, x1, y1):
+	w, h = (x1 - x0), (y1 - y0)
+	dw, dh = w/2, h/2
+	if h > w:
+		x0 = int(x0 + dw - dh)
+		x1 = int(x1 - dw + dh)
+	else:
+		y0 = int(y0 + dh - dw)
+		y1 = int(y1 - dh + dw)
+	return x0, y0, x1, y1
+	
+def get_dir(src_point, rot_rad):
+	sn, cs = np.sin(rot_rad), np.cos(rot_rad)
+
+	src_result = [0, 0]
+	src_result[0] = src_point[0] * cs - src_point[1] * sn
+	src_result[1] = src_point[0] * sn + src_point[1] * cs
+
+	return src_result
+	
+def get_3rd_point(a, b):
+	direct = a - b
+	return b + np.array([-direct[1], direct[0]], dtype=np.float32)
+	
+def get_affine_transform(center,
+                         scale,
+                         rot,
+                         output_size,
+                         shift=np.array([0, 0], dtype=np.float32),
+                         inv=0):
+	if not isinstance(scale, np.ndarray) and not isinstance(scale, list):
+		scale = np.array([scale, scale], dtype=np.float32)
+
+	scale_tmp = scale
+	src_w = scale_tmp[0]
+	dst_w = output_size[0]
+	dst_h = output_size[1]
+
+	rot_rad = np.pi * rot / 180
+	src_dir = get_dir([0, src_w * -0.5], rot_rad)
+	dst_dir = np.array([0, dst_w * -0.5], np.float32)
+
+	src = np.zeros((3, 2), dtype=np.float32)
+	dst = np.zeros((3, 2), dtype=np.float32)
+	src[0, :] = center + scale_tmp * shift
+	src[1, :] = center + src_dir + scale_tmp * shift
+	dst[0, :] = [dst_w * 0.5, dst_h * 0.5]
+	dst[1, :] = np.array([dst_w * 0.5, dst_h * 0.5], np.float32) + dst_dir
+
+	src[2:, :] = get_3rd_point(src[0, :], src[1, :])
+	dst[2:, :] = get_3rd_point(dst[0, :], dst[1, :])
+
+	if inv:
+		trans = cv2.getAffineTransform(np.float32(dst), np.float32(src))
+	else:
+		trans = cv2.getAffineTransform(np.float32(src), np.float32(dst))
+
+	return trans
+	
+def get_K_crop_resize(box, K_orig, resize_shape):
+
+	center = np.array([(box[0] + box[2]) / 2., (box[1] + box[3]) / 2.])
+	scale = np.array([box[2] - box[0], box[3] - box[1]]) # w, h
+
+	resize_h, resize_w = resize_shape
+	trans_crop = get_affine_transform(center, scale, 0, [resize_w, resize_h])
+	trans_crop_homo = np.concatenate([trans_crop, np.array([[0, 0, 1]])], axis=0)
+
+	if K_orig.shape == (3, 3):
+		K_orig_homo = np.concatenate([K_orig, np.zeros((3, 1))], axis=-1)
+	else:
+		K_orig_homo = K_orig.copy()
+	assert K_orig_homo.shape == (3, 4)
+
+	K_crop_homo = trans_crop_homo @ K_orig_homo # [3, 4]
+	K_crop = K_crop_homo[:3, :3]
+
+	return K_crop, K_crop_homo
+    
+def get_image_crop_resize(image, box, resize_shape):
+	center = np.array([(box[0] + box[2]) / 2., (box[1] + box[3]) / 2.])
+	scale = np.array([box[2] - box[0], box[3] - box[1]])
+
+	resize_h, resize_w = resize_shape
+	trans_crop = get_affine_transform(center, scale, 0, [resize_w, resize_h])
+	image_crop = cv2.warpAffine(image, trans_crop, (resize_w, resize_h), flags=cv2.INTER_LINEAR)
+
+	trans_crop_homo = np.concatenate([trans_crop, np.array([[0, 0, 1]])], axis=0)
+	return image_crop, trans_crop_homo
+	
+def crop_img_by_bbox(origin_img, bbox, K=None, crop_size=512):
+	x0, y0 = bbox[0], bbox[1]
+	x1, y1 = bbox[2], bbox[3]
+
+	resize_shape = np.array([y1 - y0, x1 - x0])
+	if K is not None:
+		K_crop, K_crop_homo = get_K_crop_resize(bbox, K, resize_shape)
+	image_crop, trans1 = get_image_crop_resize(origin_img, bbox, resize_shape)
+
+
+	bbox_new = np.array([0, 0, x1 - x0, y1 - y0])
+	resize_shape = np.array([crop_size, crop_size])
+	if K is not None:
+		K_crop, K_crop_homo = get_K_crop_resize(bbox_new, K_crop, resize_shape)
+	image_crop, trans2 = get_image_crop_resize(image_crop, bbox_new, resize_shape)
+	
+	return image_crop, K_crop if K is not None else None
+
+
+
+def crop_img_without_bbox(origin_img, bbox, K=None, crop_size=512):
+	x0, y0 = bbox[0], bbox[1]
+	x1, y1 = bbox[2], bbox[3]
+
+	resize_shape = np.array([y1 - y0, x1 - x0])
+	image_crop, trans1 = get_image_crop_resize(origin_img, bbox, resize_shape)
+
+
+	bbox_new = np.array([0, 0, x1 - x0, y1 - y0])
+	resize_shape = np.array([crop_size, crop_size])
+	image_crop, trans2 = get_image_crop_resize(image_crop, bbox_new, resize_shape)
+	
+	return image_crop
+
+
+
+
